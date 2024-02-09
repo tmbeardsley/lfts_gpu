fe49d09d3d3d5eabb0fa71daf8baea39ddb736b9
Tom Beardsley <89028856+tmbeardsley@users.noreply.github.com>
2024-02-09 15:21:57 -0500

    Update fts_gpu.cu
    Command line argument for the input file.
    <chrono> for timing.

--- fts_gpu.cu
+++ fts_gpu.cu
@@ -5,30 +5,41 @@
 // and finally output the system's energy
 // #######################################################################
 
+#include <string>
 #include <iostream>
 #include "lfts_simulation.h"
+#include <chrono>
 
 using namespace std;
 
-//------------------------------------------------------------
-int main ()
+int main (int argc, char *argv[])
 {
-    time_t t;
+    // Get input file name from command-line argument
+    if (argc != 2) {
+        cout << "Please supply a single input file name as a command-line argument." << endl << endl;
+        exit(1);
+    }
+    string inputFile(argv[1]);
 
     // New lfts_simulation object with input file name specified
-    lfts_simulation *lfts_sim = new lfts_simulation("input");
+    // and 512 threads per block on the gpu
+    lfts_simulation *lfts_sim = new lfts_simulation("input", 512);
     
     // Time the equilibration period
-    cout << "Starting Equilibrating..." << endl;
-    t = time(NULL);
+    cout << "Starting Equilibration..." << endl;
+    auto start = std::chrono::steady_clock::now();
     lfts_sim -> equilibrate();
-    cout << "Equlibration time = " << time(NULL)-t << "secs" << endl << endl;
+    auto end = std::chrono::steady_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+    cout << "Equlibration time = " << duration.count() << "secs" << endl << endl;
 
     // Time the statistics gathering period
     cout << "Starting Statistics..." << endl;
-    t = time(NULL);
+    start = std::chrono::steady_clock::now();
     lfts_sim -> statistics();
-    cout << "Statistics time = " << time(NULL)-t << "secs" << endl << endl;
+    end = std::chrono::steady_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+    cout << "Statistics time = " << duration.count() << "secs" << endl << endl;
 
     // Output the final energy of the system
     cout.precision(6);
